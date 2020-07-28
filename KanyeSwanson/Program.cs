043f612fd8d98237f85655279c94455906c842c6
John Huffman <jmhuffman37@gmail.com>
2020-07-28 12:14:53 -0500

    Add project files.

---
+++ KanyeSwanson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace KanyeSwanson
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+        }
+    }
+}
